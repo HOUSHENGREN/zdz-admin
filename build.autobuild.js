88dad8a72f83547a4c7da2a353d6b50d33f69cc7
zhangdezheng <2549626703@qq.com>
2024-07-17 21:16:50 +0800

    test shell

---
+++ build.autobuild.js
@@ -0,0 +1,9 @@
+const nodeConfig = {
+  NODE_BUILD_PATH: '',
+  NODE_DIST_PATH: '',
+  VERSION: '1.79.1',
+  PROJECT_NAME: 'factoring-credit-h5',
+  ACCESS_PATH: ''
+}
+
+module.exports = nodeConfig
